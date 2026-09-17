9337e453d316aab22fb3e52dc324b16d2a526755
Yu Tae I <tiyuuuu9@gmail.com>
2026-09-17 22:20:51 +0900

    [level 0] Title: 수 조작하기 2, Time: 7.54 ms, Memory: 53.6 MB -BaekjoonHub

---
+++ JavaScript/프로그래머스/0/181925. 수 조작하기 2/수 조작하기 2.js
@@ -0,0 +1,17 @@
+function solution(numLog) {
+    let answer = [];
+    
+    for (let i = 0; i < numLog.length; i++ ) {
+        if (numLog[i + 1] - numLog[i] === 1) {
+            answer.push('w')
+        } else if (numLog[i + 1] - numLog[i] === -1) {
+            answer.push('s')
+        } else if (numLog[i + 1] - numLog[i] === 10) {
+            answer.push('d')
+        } else if (numLog[i + 1] - numLog[i] === -10) {
+            answer.push('a')
+        }
+    }
+    
+    return answer.join('');
+}
